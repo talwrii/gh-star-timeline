dd2542777ccc0601c08544ed93ab261bcf0f792d
Tal Wrii <talwrii@gmail.com>
2025-04-26 17:12:09 +0200

    Fix bug when fetching. direction and sort are not implemented on stargazers - misread documentation from another method and parameters were being silently ignored. Use another method for fetching

--- gh_star_timeline/main.py
+++ gh_star_timeline/main.py
@@ -12,7 +12,7 @@ from typing import Tuple
 from . import timeseries
 
 PARSER = argparse.ArgumentParser(description='Maintain a timeline of the number of stars for a repo')
-PARSER.add_argument('repo')
+PARSER.add_argument('repo', nargs="?")
 PARSER.add_argument('--user', action='store_true', help="Get information about all repos for a user")
 PARSER.add_argument('-n', '--no-fetch', action='store_false', help="Get information about all repos for a user", default=True, dest="fetch")
 PARSER.add_argument('--path', action='store_true', default=False, help='Print the path the data directory')
@@ -42,8 +42,14 @@ def main():
 
 
     if args.path:
-        print(str(STATS_DIR))
+        if args.repo:
+            print(repo_path(args.repo))
+        else:
+            print(str(STATS_DIR))
         return
+
+    if args.repo is None:
+        print("Must provide a repository")
 
     if args.user:
         user = args.repo
@@ -57,8 +63,7 @@ def main():
     else:
         repos = [args.repo]
 
-    if args.fetch:
-        logging.info("Getting stars for repo...")
+
     percent = 0
     repo_stars = {}
     all_stars = []
@@ -77,13 +82,14 @@ def main():
 
         stars = list(read_repo(repo))
         all_stars.extend(stars)
+
         num_stars = sum(event_stars(e) for e in stars)
         if args.fetch:
-            stars_for_repo = github_star_count(repo)
+            star_count = github_star_count(repo)
 
-            if num_stars != stars_for_repo:
+            if num_stars != star_count:
                 # Some stars have been removed refetch
-                logging.info('Stargazers and stars do not match for %r. Finding removed stars...', repo)
+                logging.info('Stargazers counts (%s) and stars (%s) do not match for %r. Finding removed stars...', num_stars, star_count, repo)
 
                 new_gazers = set(s["user"] for s in StarFetcher(repo).fetch())
                 current_gazers = set(gazers_from_events(read_repo(repo)))
@@ -92,6 +98,10 @@ def main():
                     remove_star(x)
 
                 num_stars = sum(event_stars(e) for e in read_repo(repo))
+
+
+            if num_stars != star_count:
+                raise Exception("Stars still do not match after removing stars")
 
         repo_stars[repo] = num_stars
 
@@ -129,9 +139,11 @@ def main():
             for k, v in repo_stars.items():
                 print(k, v)
 
+def repo_path(repo):
+    return STATS_DIR / (repo.replace("/", "--") + ".json")
 
 def read_repo(repo):
-    path = STATS_DIR / (repo.replace("/", "--") + ".json")
+    path = repo_path(repo)
     if not path.exists():
         return []
     with path.open() as stream:
@@ -139,8 +151,7 @@ def read_repo(repo):
             yield dict(repo=repo, **json.loads(line))
 
 def add_star(repo, star):
-    path = STATS_DIR / (repo.replace("/", "--") + ".json")
-    with path.open("a") as stream:
+    with repo_path(repo).open("a") as stream:
         stream.write(json.dumps(star) + "\n")
 
 def remove_star(user):
@@ -177,10 +188,13 @@ def github_star_count(repo):
 
         return data["stargazers_count"]
 
-def fetch_stargazers_page(repo, page):
+def fetch_stargazers_page(repo:str, page:int, page_size:int) -> list:
+    if page < 1:
+        raise Exception('Pages must be 1 or larger (indexing starts at 1)')
     for i in range(5):
         user, repo_name = repo.split("/")
-        command = ["gh",  "api", f'repos/{user}/{repo_name}/stargazers?per_page=10&direction=desc&page={page}', "-H", 'Accept: application/vnd.github.v3.star+json']
+        command = ["gh",  "api", f'repos/{user}/{repo_name}/stargazers?per_page={page_size}&page={page}', "-H", 'Accept: application/vnd.github.v3.star+json']
+
         try:
             raw = subprocess.check_output(command)
         except subprocess.CalledProcessError:
@@ -188,7 +202,10 @@ def fetch_stargazers_page(repo, page):
                 raise
             time.sleep(1)
             continue
-        return json.loads(raw)
+
+        result = json.loads(raw)
+        logging.debug('Fetched page: %r, size: %r, stars:%s', page, page_size, len(result))
+        return result
 
 
 def fetch_user_repos_page(user, i):
@@ -209,18 +226,47 @@ class StarFetcher:
     def __init__(self, repo):
         self.num_pages = 0
         self.repo = repo
+        self.page_size = 10
 
     def fetch(self):
         for i in itertools.count(1):
-            updates = fetch_stargazers_page(self.repo, i)
+            updates = fetch_stargazers_page(self.repo, i, page_size=self.page_size)
             self.num_pages = i
             if not updates:
                 break
 
             for update in updates:
-                star = {"timestamp": update["starred_at"], "user": update["user"]["login"], "event": "added"}
+                star = parse_star(update)
+
                 yield star
 
+    def fetch_page(self, page):
+        updates = fetch_stargazers_page(self.repo, page, self.page_size)
+        return list(map(parse_star, updates))
+
+    def cursor(self, page=None, index=None):
+        if page is None:
+            page = index // self.page_size + 1
+        else:
+            assert index is None
+
+        return Cursor(self, page)
+
+class Cursor:
+    def __init__(self, fetcher, page):
+        self.fetcher = fetcher
+        self.page = page
+
+    def fetch(self):
+        return self.fetcher.fetch_page(page=self.page)
+
+    def prev(self):
+        if self.page <= 1:
+            return None
+        return Cursor(self.fetcher, self.page - 1)
+
+    def next(self):
+        return Cursor(self.fetcher, self.page + 1)
 
 
 def fetch_user_repos(user):
@@ -232,19 +278,60 @@ def fetch_user_repos(user):
         for x in updates:
             yield x["full_name"]
 
-
 def fetch(repo):
-    existing_keys = set(key(d) for d in read_repo(repo))
+    star_count = 0
+    existing_keys = set()
+    timestamp = None
+    for d in read_repo(repo):
+        star_count += 1
+        existing_keys.add(key(d))
+        timestamp = d["timestamp"]
+
     stars_to_add = []
 
+    # guess at page with current stars
+    #  search up and down until existing match
+
     fetcher = StarFetcher(repo)
-    for star in fetcher.fetch():
-        if key(star) in existing_keys:
+
+    guess_cursor = fetcher.cursor(index=star_count)
+
+    pages_fetched = 0
+
+    cursor = guess_cursor
+    while cursor:
+        updates = cursor.fetch()
+        pages_fetched += 1
+        cursor =  cursor.prev()
+
+        if not updates:
             break
 
-        stars_to_add.append(star)
+        for x in reversed(updates):
+            if timestamp and x["timestamp"] < timestamp:
+                break
 
-    logging.debug('Got %r pages while fetching starts for %r', fetcher.num_pages, repo)
+            if key(x) in existing_keys:
+                break
+
+            stars_to_add.append(x)
+
+    cursor = guess_cursor.next()
+    while cursor:
+        updates = cursor.fetch()
+        pages_fetched += 1
+        cursor = cursor.next()
+        if not updates:
+            break
+
+        for x in updates:
+            if timestamp and x["timestamp"] < timestamp:
+                continue
+            stars_to_add.append(x)
+
+    logging.debug('Got %r pages while fetching starts for %r', pages_fetched, repo)
+
+    print(len(stars_to_add))
 
     for star in reversed(stars_to_add):
         add_star(repo, star)
@@ -288,3 +375,7 @@ def gazers_from_events(events):
             raise Exception(f"{k} has a strange number of stars {count}")
 
     return [x for x, count in user_stars.items() if count == 1]
+
+
+def parse_star(update):
+    return {"timestamp": update["starred_at"], "user": update["user"]["login"], "event": "added"}
