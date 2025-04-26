f5ac55636ae33b3cdc2c502872aa3f62d54e30c9
Tal Wrii <talwrii@gmail.com>
2025-04-26 18:37:15 +0200

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import distutils.core
 
 setuptools.setup(
     name='gh-star-timeline',
-    version="1.2.1",
+    version="2.0.0",
     author='@readwithai',
     long_description_content_type='text/markdown',
     author_email='talwrii@gmail.co',
